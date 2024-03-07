e0acf6dff5447587477f90c4de509c3eebc05fc2
KirtiJha <kirti.jha2025@gmail.com>
2024-03-07 20:35:55 +0530

    Add new files

---
+++ script.js
@@ -0,0 +1,17 @@
+let searchForm = document.querySelector('.search_form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle("active");
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart_item_container');
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle("active");
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    // navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
