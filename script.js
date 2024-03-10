6df2e4757b8cce31a4068ebfe4b256e9d481f103
KirtiJha <kirti.jha2025@gmail.com>
2024-03-10 20:03:54 +0530

    adding responsive property

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+let sidebar = document.querySelector('.sidebar');
+document.querySelector('#menu-btn').onclick = () => {
+    sidebar.classList.toggle("active");
+    cartItem.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
 let searchForm = document.querySelector('.search_form');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle("active");
     cartItem.classList.remove('active');
+    sidebar.classList.remove('active');
 }
 
 let cartItem = document.querySelector('.cart_item_container');
 document.querySelector('#cart-btn').onclick = () => {
     cartItem.classList.toggle("active");
     searchForm.classList.remove('active');
+    sidebar.classList.remove('active');
 }
 
 window.onscroll = () => {
-    // navbar.classList.remove('active');
+    sidebar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
